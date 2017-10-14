cbbb7c20dbbda53dff22abcf6e5cdb98614cdca9
RodionOrets <rodiksb@gmail.com>
2017-10-14 11:13:28 +0300

    sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -8,4 +8,11 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS roles (
   id INT PRIMARY KEY AUTO_INCREMENT,
   rolename VARCHAR(30) NOT NULL UNIQUE
+)
+
+CREATE TABLE IF NOT EXISTS users_to_roles (
+  user_to_role_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL, role_id INT NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES users(id),
+   FOREIGN KEY(role_id) REFERENCES roles(role_id)
 );
