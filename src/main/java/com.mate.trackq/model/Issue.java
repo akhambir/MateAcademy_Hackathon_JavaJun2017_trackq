539d697656ca8d542bcfb50de0792f7a3621f00d
shahabsohrabi <shb.sohrabi@gmail.com>
2017-10-14 12:58:18 +0300

    Model- second attempt

--- src/main/java/com.mate.trackq/model/Issue.java
+++ src/main/java/com.mate.trackq/model/Issue.java
@@ -19,6 +19,43 @@ public class Issue {
     private User tssignee;
     private User reporter;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public List<Status> getStatusList() {
+        return statusList;
+    }
+
+    public void setStatusList(List<Status> statusList) {
+        this.statusList = statusList;
+    }
+
+    public User getTssignee() {
+        return tssignee;
+    }
+
+    public void setTssignee(User tssignee) {
+        this.tssignee = tssignee;
+    }
+
+    public User getReporter() {
+        return reporter;
+    }
+
+    public void setReporter(User reporter) {
+        this.reporter = reporter;
+    }
 }
