d3bc3695b699298b62be284a9a4ea6d03ae8a2c4
RodionOrets <rodiksb@gmail.com>
2017-10-14 12:18:37 +0300

    Merge https://github.com/akhambir/MateAcademy_Hackathon_JavaJun2017_trackq
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com.mate.trackq/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.mate.trackq.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.mate.trackq")
+public class AppConfig {
+
+
+
+}
