6f7b5c7dca7b726c8bdcb2ff5bef3c557b97a1bd
ponaguynik <ponaguynik@gmail.com>
2017-10-14 13:35:36 +0300

    Packages

--- src/main/java/com.mate.trackq/model/User.java
+++
@@ -1,4 +0,0 @@
-package com.mate.trackq.model;
-
-public class User {
-}
--- src/main/java/com/mate/trackq/dao/UserDao.java
+++ src/main/java/com/mate/trackq/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.mate.trackq.dao;
 
-import com.mate.trackq.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDao extends CrudRepository<User, Long> {
