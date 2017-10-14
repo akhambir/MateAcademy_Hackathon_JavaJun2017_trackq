9ce01875e87cc7b25382b83a3af6ada726502a6b
IrohTwelve <osoleksandr@gmail.com>
2017-10-14 17:39:12 +0300

    added Mail Service

--- src/main/java/com/mate/trackq/controllers/UserController.java
+++ src/main/java/com/mate/trackq/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.mate.trackq.controllers;
 
 import com.mate.trackq.model.User;
+import com.mate.trackq.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class UserController {
 //    private final UserService userService;
+
+    @Autowired
+    private MailService mailService;
 
     @Autowired
 //    public UserController(UserService userService) {
@@ -27,4 +33,13 @@ public class UserController {
 //        user=userService.findUser(user.getUserName(), TomcatFactory.md5Custom(user.getPassword(),logger));
         return new ModelAndView("home", "user", user);
     }
+
+
+    @RequestMapping("/")
+    public ModelAndView register(HttpServletRequest request, @ModelAttribute User user) {
+        ModelAndView mv = new ModelAndView();
+        mailService.sendConfirmRegistrationEmail(user, request.getServerName());
+        return mv;
+    }
+
 }
