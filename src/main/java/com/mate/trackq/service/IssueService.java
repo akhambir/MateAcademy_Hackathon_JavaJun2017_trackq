cd265d21875741727c08dc7a41ab59f229ff2cc6
IrohTwelve <osoleksandr@gmail.com>
2017-10-15 12:54:26 +0300

    Changed Sql table

--- src/main/java/com/mate/trackq/service/IssueService.java
+++ src/main/java/com/mate/trackq/service/IssueService.java
@@ -1,7 +1,9 @@
 package com.mate.trackq.service;
 
 import com.mate.trackq.model.Issue;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface IssueService {
     void createIssue(Issue issue);
 }
